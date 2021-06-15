99946ad0114a568c72795cd4da1fc667383a9993
voodoocode <MaximilianScherer@posteo.net>
2021-06-15 10:28:56 -0400

    packaged typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='finn',
-    version='0.1.1',
+    version='0.1.2',
     description='Toolbox for the analysis of electrophysiological data',
     long_description='A Python Toolbox for the analysis of electrophysiological data',
     author='Maximilian Scherer',
