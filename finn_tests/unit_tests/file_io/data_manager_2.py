5aeddb64d1c3299a7e8036aa15eee0838ca49e3e
voodoocode <MaximilianScherer@posteo.net>
2021-09-15 20:41:18 -0400

    Updated test cases to new pathing

--- finn_tests/unit_tests/file_io/data_manager_2.py
+++ finn_tests/unit_tests/file_io/data_manager_2.py
@@ -4,15 +4,15 @@ Created on Dec 30, 2020
 @author: voodoocode
 '''
 
-import file_io.data_manager_legacy
+import finn.file_io.data_manager
 
 data = [[[1, 2], [3, 4]], [[1, 2], [3, 4], ["a", "b", "c"], ["x", "x"]]]
 #data = [[1], {'a' : [2], 2 : [3]}]
 #data = 2
 
-file_io.data_manager.save(data, "/home/voodoocode/Downloads/test_file", max_depth = 2)
+finn.file_io.data_manager.save(data, "/home/voodoocode/Downloads/test_file", max_depth = 2)
 
-data2 = file_io.data_manager.load("/home/voodoocode/Downloads/test_file")
+data2 = finn.file_io.data_manager.load("/home/voodoocode/Downloads/test_file")
 
 print(data == data2)
 
