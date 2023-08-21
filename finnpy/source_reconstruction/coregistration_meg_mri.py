43adbc728c9173f09b70a2a3c44ad345b87df1b2
Maximilian Scherer (VoodooCode) <MaximilianScherer@posteo.net>
2023-08-21 16:16:53 -0400

    Updated documentation

--- finnpy/source_reconstruction/coregistration_meg_mri.py
+++ finnpy/source_reconstruction/coregistration_meg_mri.py
@@ -4,7 +4,23 @@ Created on Oct 12, 2022
 @author: voodoocode
 '''
 
+import functools
 
+import numpy as np
+import scipy.optimize
+import scipy.spatial
+import scipy.linalg
 
-import mayavi.mlab
+import nibabel.freesurfer
+
+import finnpy.source_reconstruction.utils as finnpy_utils
+import os
+
+import mne
+
+import warnings
+
+import shutil
+
+import finnpy.file_io.data_manager as dm
 
